f7f038998fa068b6f61a758405bd1c0d0010b09c
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-04 17:12:39 +0530

    Update two_sum.cpp

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -7,3 +7,21 @@
   You may assume that each input would have exactly one solution, and you may not use the same element twice.
   You can return the answer in any order.
 */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> arr; 
+        int size;
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                int sum = nums[i] + nums[j];
+                if(sum == target) {
+                    arr.push_back(i);
+                    arr.push_back(j);
+                }
+            }
+        }
+        return arr;
+    }
+};
