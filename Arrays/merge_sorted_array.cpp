b0bd211056fd41c279381f680fba650ec504a91f
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-04 17:11:05 +0530

    Update merge_sorted_array.cpp

--- Arrays/merge_sorted_array.cpp
+++ Arrays/merge_sorted_array.cpp
@@ -8,3 +8,28 @@
   The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 */
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int arr[m+n];
+
+        int i=0, j=0, k=0;
+        while(i<m && j<n) {
+            if(nums1[i] < nums2[j]){
+                arr[k++] = nums1[i++];
+            } else {
+                arr[k++] = nums2[j++];
+            }
+        }
+        
+        for(; i < m; i++)
+            arr[k++] = nums1[i];
+        
+        for(; j < n; j++)
+            arr[k++] = nums2[j];
+        
+        for(int i=0; i<m+n; i++)
+            nums1[i] = arr[i];
+    }
+};
