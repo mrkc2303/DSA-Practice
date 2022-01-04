5696e1b77bec708274c89cfe91e8ba406817a6c4
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-04 17:09:18 +0530

    Update build_array_from_permutations.cpp

--- Arrays/build_array_from_permutations.cpp
+++ Arrays/build_array_from_permutations.cpp
@@ -7,3 +7,16 @@
   Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
   A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
 */
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> temp;
+        for(int i=0; i<nums.size(); i++) {
+            int tempa = nums[i];
+            temp.push_back(nums[tempa]);
+            // temp[i] = nums[tempa];
+        }
+        return temp;
+    }
+};
