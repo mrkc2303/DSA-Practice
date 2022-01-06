e8652f312cdc184d66f51225a1dac25c737fe3ae
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-07 00:32:12 +0530

    Update maximum_number_of_words_found_in_a_sentence.cpp

--- Arrays/maximum_number_of_words_found_in_a_sentence.cpp
+++ Arrays/maximum_number_of_words_found_in_a_sentence.cpp
@@ -8,3 +8,23 @@
   You are given an array of strings sentences, where each sentences[i] represents a single sentence.
   Return the maximum number of words that appear in a single sentence.
 */
+
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxCount = 0;
+        for(int i=0; i<sentences.size(); i++) {
+            int count=1;
+            // for(int j=0; j<sentences[i].size(); j++) {
+            //     if(sentences[i][j] == ' ')
+            //         count++;
+            // }
+            count = std::count(sentences[i].begin(),sentences[i].end(),' ');
+            count++;
+            maxCount = max(maxCount, count);
+        }
+        
+
+        return maxCount;
+    }
+};
