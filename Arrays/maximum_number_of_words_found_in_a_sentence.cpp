5019ad302f0d4df8a3ac7f7e376426f9fe641273
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-07 00:31:55 +0530

    Create maximum_number_of_words_found_in_a_sentence.cpp

---
+++ Arrays/maximum_number_of_words_found_in_a_sentence.cpp
@@ -0,0 +1,10 @@
+/*                    LEETCODE PROBLEM: 2114
+              Maximum Number of Words Found in Sentences
+                   
+  https://leetcode.com/problems/move-zeroes/
+  
+  
+  A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+  You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+  Return the maximum number of words that appear in a single sentence.
+*/
