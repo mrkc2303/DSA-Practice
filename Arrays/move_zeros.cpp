68a6acf9e7f5e2b321eb6b27a56b0600eb657a67
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-06 23:30:09 +0530

    Update move_zeros.cpp

--- Arrays/move_zeros.cpp
+++ Arrays/move_zeros.cpp
@@ -8,3 +8,18 @@
   Note that you must do this in-place without making a copy of the array
 */
 
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j=0;
+        for(int i=0; i < n; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+};
