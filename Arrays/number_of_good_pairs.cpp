f5de221c9260004201962169bfb617f1ba9a0850
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-10 22:36:51 +0530

    Create number_of_good_pairs.cpp

---
+++ Arrays/number_of_good_pairs.cpp
@@ -0,0 +1,10 @@
+/*                    LEETCODE PROBLEM: 1512
+                       Number of Good Pairs
+                   
+  https://leetcode.com/problems/number-of-good-pairs/
+  
+  
+  Given an array of integers nums, return the number of good pairs.
+  A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+*/
+
