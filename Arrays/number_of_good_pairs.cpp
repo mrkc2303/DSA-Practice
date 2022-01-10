b623af7a137f82b14167e26e3d208ca13f80d172
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-10 22:37:13 +0530

    Update number_of_good_pairs.cpp

--- Arrays/number_of_good_pairs.cpp
+++ Arrays/number_of_good_pairs.cpp
@@ -8,3 +8,16 @@
   A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 */
 
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int pairs=0;
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                if(nums[i] == nums[j])
+                    pairs++;
+            }
+        }
+        return pairs;
+    }
+};
