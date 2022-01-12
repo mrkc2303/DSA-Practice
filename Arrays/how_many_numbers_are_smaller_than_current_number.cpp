3ff7bcc2fab7b1ffde7f0bc0147d83e354216cc0
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-12 16:56:20 +0530

    Update how_many_numbers_are_smaller_than_current_number.cpp

--- Arrays/how_many_numbers_are_smaller_than_current_number.cpp
+++ Arrays/how_many_numbers_are_smaller_than_current_number.cpp
@@ -1,3 +1,14 @@
+
+/*                    LEETCODE PROBLEM: 1365
+        How Many Numbers Are Smaller Than the Current Number
+                   
+  https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+  
+  
+  Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+  Return the answer in an array.
+*/
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
