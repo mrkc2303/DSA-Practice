fa7fda9ce77a4c84030daed52b0dbab38acbbaa1
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-21 20:32:31 +0530

    Create count_items_matching_a_rule.cpp

---
+++ Arrays/count_items_matching_a_rule.cpp
@@ -0,0 +1,14 @@
+/*                    LEETCODE PROBLEM: 1773
+                   Count Items Matching a Rule
+                   
+  https://leetcode.com/problems/count-items-matching-a-rule/
+  
+  
+  You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+  The ith item is said to match the rule if one of the following is true:
+
+  ruleKey == "type" and ruleValue == typei.
+  ruleKey == "color" and ruleValue == colori.
+  ruleKey == "name" and ruleValue == namei.
+  Return the number of items that match the given rule.
+*/
