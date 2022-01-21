33656c4923de9bbbe3efdf521d439e57f06ccf7a
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-21 20:31:29 +0530

    Create shuffle_string.cpp

---
+++ Arrays/shuffle_string.cpp
@@ -0,0 +1,22 @@
+/*                    LEETCODE PROBLEM: 1528
+                          Shuffle String
+                   
+  https://leetcode.com/problems/number-of-good-pairs/
+  
+  
+  You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+  Return the shuffled string.
+*/
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string ans=s;
+        
+        for(int i=0; i<indices.size(); i++) {
+            int temp = indices[i];
+            ans[temp] = s[i];
+        }
+        return ans;
+    }
+};
