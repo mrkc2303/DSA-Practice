1314ecffe8994705fa3e40588d2364f9e561b673
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-22 20:29:55 +0530

    Update XOR_operation_in_an_array.cpp

--- Arrays/XOR_operation_in_an_array.cpp
+++ Arrays/XOR_operation_in_an_array.cpp
@@ -8,3 +8,14 @@
   Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
   Return the bitwise XOR of all elements of nums.
 */
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int x = start;
+        for(int i=1; i < n; i++) {
+            x = x ^ (start + 2 * i);
+        }
+        return x;
+    }
+};
