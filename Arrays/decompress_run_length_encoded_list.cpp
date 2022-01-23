d0bcb2791c0d799dfd2dacdbaa6e54bba10f8cc0
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-01-23 21:03:15 +0530

    Update decompress_run_length_encoded_list.cpp

--- Arrays/decompress_run_length_encoded_list.cpp
+++ Arrays/decompress_run_length_encoded_list.cpp
@@ -8,3 +8,16 @@
   Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
   Return the decompressed list.
 */
+
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0; i < nums.size(); i=i+2) {
+            for(int j=0; j < nums[i]; j++) {
+                ans.push_back(nums[i+1]);
+            }
+        }
+        return ans;
+    }
+};
