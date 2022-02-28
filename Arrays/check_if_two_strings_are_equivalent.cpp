a44af08d42ffb4a38645b04679e57fdf50a866dc
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2022-02-28 19:41:04 +0530

    Create check_if_two_strings_are_equivalent.cpp

---
+++ Arrays/check_if_two_strings_are_equivalent.cpp
@@ -0,0 +1,11 @@
+/*                    LEETCODE PROBLEM: 1662
+               Check If Two String Arrays are Equivalent
+
+  https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+  
+  
+  Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+  A string is represented by an array if the array elements concatenated in order forms the string.
+
+
+*/
