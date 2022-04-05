ba2ef55e9c0104d24e148447ad509f9591ee5be9
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-04-05 20:21:16 +0530

    Added Files for Find N Unique Integers Sum up to Zero

--- Arrays/find-n-unique-integers-sum-up-to-zero.cpp
+++ Arrays/find-n-unique-integers-sum-up-to-zero.cpp
@@ -23,3 +23,18 @@
 
 */
 
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        cout << n/2;
+        vector<int> ans;
+        if(n%2 != 0) {
+            ans.push_back(0);
+        }
+        for(int i=1; i <= n/2; i++) {
+            ans.push_back(i);
+            ans.push_back(i*-1);
+        }
+        return ans;
+    }
+};
