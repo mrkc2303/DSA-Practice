0bbb8b3784aa503f5efe84105d09978928777ced
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-05-06 20:23:46 +0530

    Added Files

--- Arrays/final-prices-with-a-special-discount-in-a-shop.cpp
+++ Arrays/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -29,22 +29,3 @@
 	1 <= prices[i] <= 10^3
 
 */
-
-class Solution {
-public:
-    vector<int> finalPrices(vector<int>& prices) {
-        vector<int> ans;
-        for(int i=0; i < prices.size(); i++) {
-            int dis=0;
-            for(int j=i+1; j < prices.size(); j++) {
-                if(prices[j] <= prices[i]) {
-                    dis = prices[j];
-                    break;
-                }
-            }
-            ans.push_back(prices[i] - dis);
-        }
-        
-        return ans;
-    }
-};
