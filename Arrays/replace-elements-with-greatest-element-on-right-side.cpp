db9ac2e0242e35d77189cea21a8def9d3d675abe
Kanishk Chhabra <kanishkchhabra23@gmail.com>
2022-05-07 16:19:47 +0530

    Added Code for few files

--- Arrays/replace-elements-with-greatest-element-on-right-side.cpp
+++ Arrays/replace-elements-with-greatest-element-on-right-side.cpp
@@ -28,6 +28,29 @@
 
 */
 
+/*
+					APPROACH-1
+					BRUTE FORCE
+
+	class Solution {
+	public:
+			vector<int> replaceElements(vector<int>& arr) {
+					vector<int> ans;
+					for(int i=0; i < arr.size()-1; i++) {
+							int largest=0;
+							for(int j=i+1; j < arr.size(); j++) {
+									largest = max(largest, arr[j]);
+							}
+							ans.push_back(largest);
+					}
+							
+					ans.push_back(-1);
+					return ans;
+			}
+	};
+
+*/
+
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
